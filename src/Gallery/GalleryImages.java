a22141a7c08779872aaafbcc6cdabe534d66f6e7
raphy <lokiraphael@hotmail.com>
2019-06-09 18:27:23 +0200

    clean code

--- src/Gallery/GalleryImages.java
+++ src/Gallery/GalleryImages.java
@@ -7,7 +7,6 @@ import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
